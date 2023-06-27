d47beb291486ee834ef59908907c55bf3dbff331
davemaa17 <91053980+davemaa17@users.noreply.github.com>
2023-06-27 23:22:17 +0700

    Update save_contact.js

--- scripts/save_contact.js
+++ scripts/save_contact.js
@@ -1,27 +1,33 @@
 function saveContact() {
-    const contact = navigator.contacts.create();
-    contact.displayName = "Dave Machario";
-    contact.nickname = "Dave";
-  
-    const name = new ContactName();
-    name.givenName = "Dave";
-    name.familyName = "Machario";
-    contact.name = name;
-  
-    const email = new ContactField("email", "davemachario@gmail.com", false);
-    contact.emails = [email];
-  
-    const phone = new ContactField("phone", "+85293457707", true);
-    contact.phoneNumbers = [phone];
-  
-    const saveOptions = new ContactSaveOptions();
-    saveOptions.contactSaveSuccess = function (contact) {
-      alert("Contact saved successfully!");
-    };
-    saveOptions.contactSaveError = function (error) {
-      alert("Error saving contact: " + error);
-    };
-  
-    contact.save(saveOptions);
+  const contact = {
+    name: "Dave Machario",
+    email: "davemachario@gmail.com",
+    companyEmail: "dave.machario@sekologistics.com",
+    phone: "+85293457707"
+  };
+
+  if (navigator.contacts) {
+    // For mobile devices with contact support
+    const contactObject = navigator.contacts.create(contact);
+    contactObject.save();
+  } else {
+    // For other devices or browsers
+    const contactVCard = [
+      `BEGIN:VCARD`,
+      `VERSION:3.0`,
+      `N:${contact.name}`,
+      `EMAIL;TYPE=INTERNET:${contact.email}`,
+      `EMAIL;TYPE=WORK:${contact.companyEmail}`,
+      `TEL;TYPE=CELL:${contact.phone}`,
+      `END:VCARD`
+    ].join("\n");
+
+    const encodedVCard = encodeURIComponent(contactVCard);
+    const dataUri = `data:text/vcard;charset=utf-8,${encodedVCard}`;
+
+    const link = document.createElement("a");
+    link.href = dataUri;
+    link.setAttribute("download", `${contact.name}.vcf`);
+    link.click();
   }
-  
+}
