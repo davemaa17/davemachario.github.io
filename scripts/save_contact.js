70c41c448d4f809a6e75797bf7a2cccbfac83d4f
davemaa17 <91053980+davemaa17@users.noreply.github.com>
2023-06-27 23:38:59 +0700

    Update save_contact.js

--- scripts/save_contact.js
+++ scripts/save_contact.js
@@ -1,33 +1,25 @@
-function saveContact() {
-  const contact = {
-    name: "Dave Machario",
-    email: "davemachario@gmail.com",
-    companyEmail: "dave.machario@sekologistics.com",
-    phone: "+85293457707"
-  };
+function downloadVCard() {
+  const vcardData = `
+    BEGIN:VCARD
+    VERSION:3.0
+    FN:Dave Machario
+    EMAIL:davemachario@gmail.com
+    TEL;TYPE=CELL:(+852) 9345-7707
+    ORG:SEKO Logistics, Island Productions
+    TITLE:Data Analyst, Freelance Audio Editor
+    URL:https://www.linkedin.com/in/davemachario/
+    URL:https://www.instagram.com/islandproductions.id
+    URL:https://www.youtube.com/channel/UCInemueXqh0N622jrIGtmVQ
+    END:VCARD
+  `;
 
-  if (navigator.contacts) {
-    // For mobile devices with contact support
-    const contactObject = navigator.contacts.create(contact);
-    contactObject.save();
-  } else {
-    // For other devices or browsers
-    const contactVCard = [
-      `BEGIN:VCARD`,
-      `VERSION:3.0`,
-      `N:${contact.name}`,
-      `EMAIL;TYPE=INTERNET:${contact.email}`,
-      `EMAIL;TYPE=WORK:${contact.companyEmail}`,
-      `TEL;TYPE=CELL:${contact.phone}`,
-      `END:VCARD`
-    ].join("\n");
+  const blob = new Blob([vcardData], { type: 'text/vcard' });
+  const url = URL.createObjectURL(blob);
 
-    const encodedVCard = encodeURIComponent(contactVCard);
-    const dataUri = `data:text/vcard;charset=utf-8,${encodedVCard}`;
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'contact.vcf';
+  link.click();
 
-    const link = document.createElement("a");
-    link.href = dataUri;
-    link.setAttribute("download", `${contact.name}.vcf`);
-    link.click();
-  }
+  URL.revokeObjectURL(url);
 }
